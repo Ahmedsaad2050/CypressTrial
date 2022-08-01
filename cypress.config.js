2dadbd59c58331408adff860b1302faadeda86a1
Ahmed <ahmedsaad2050@gmail.com>
2022-08-01 05:37:06 +0200

    ProjectID Added

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'Ahmed',
   e2e: {
     setupNodeEvents(on, config) {
         baseUrl: 'http://localhost:1234'
