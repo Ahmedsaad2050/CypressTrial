f7ff4c7202abfb85be1a67b7d7a93ef4b3eb22c6
Ahmed <ahmedsaad2050@gmail.com>
2022-08-01 05:45:49 +0200

    Config ver1.1

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: 'Ahmed',
+  record_key: "UniqueID",
   e2e: {
     setupNodeEvents(on, config) {
         baseUrl: 'http://localhost:1234'
